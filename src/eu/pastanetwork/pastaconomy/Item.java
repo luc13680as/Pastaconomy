ec28f93936f5436b9c73bf7c188ecdf1f48f09b4
Lucas <lucas.m.jacquet@gmail.com>
2022-12-30 21:03:59 +0100

    Added a category field

--- src/eu/pastanetwork/pastaconomy/Item.java
+++ src/eu/pastanetwork/pastaconomy/Item.java
@@ -3,24 +3,30 @@ package eu.pastanetwork.pastaconomy;
 public class Item {
     private String name;
     private String description;
+    private String category;
     private int maxSlotQuantity;
 
-    public Item(String nameProvided, String descriptionProvided, int maxSlotQuantity){
+    public Item(String nameProvided, String descriptionProvided, String categoryProvided, int maxSlotQuantity){
         this.name = nameProvided;
         this.description = descriptionProvided;
+        this.category = categoryProvided;
         this.maxSlotQuantity = maxSlotQuantity;
     }
 
+    public Item(String nameProvided, String descriptionProvided, int maxSlotQuantity){
+        this(nameProvided, descriptionProvided, "default", maxSlotQuantity);
+    }
+
     public Item(String nameProvided, String descriptionProvided){
-        this(nameProvided, descriptionProvided, 64);
+        this(nameProvided, descriptionProvided, "default",64);
     }
 
     public Item(String nameProvided, int maxSlotQuantity){
-        this(nameProvided, "No description available", maxSlotQuantity);
+        this(nameProvided, "No description available", "default", maxSlotQuantity);
     }
 
     public Item(String nameProvided){
-        this(nameProvided, "No description available", 64);
+        this(nameProvided, "No description available", "default", 64);
     }
 
     public String getName(){
@@ -31,5 +37,13 @@ public class Item {
     }
     public int getMaxSlotQuantity(){
         return this.maxSlotQuantity;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
