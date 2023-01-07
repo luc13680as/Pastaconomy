4da80e93a0c918e2fe6f05a82cbcc7708d7da466
Lucas <lucas.m.jacquet@gmail.com>
2023-01-07 17:15:09 +0100

    Finalized interface for object able to create order

--- src/eu/pastanetwork/pastaconomy/IOrderPlacer.java
+++ src/eu/pastanetwork/pastaconomy/IOrderPlacer.java
@@ -1,9 +1,7 @@
 package eu.pastanetwork.pastaconomy;
 
 public interface IOrderPlacer {
-    Market market;
     public void addMarket(Market theMarket);
-    public boolean hasMarket()
-    public boolean checkOrder(Market.Order order);
-    public boolean processOrder(Market.Order order);
+    public boolean hasMarket();
+    public void processOrder(Market.Order order);
 }
