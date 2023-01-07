89da04945af871a843bb8c38aa7b8845170b66d5
Lucas <lucas.m.jacquet@gmail.com>
2023-01-08 00:22:52 +0100

    Added price for proccessOrder

--- src/eu/pastanetwork/pastaconomy/IOrderPlacer.java
+++ src/eu/pastanetwork/pastaconomy/IOrderPlacer.java
@@ -1,7 +1,10 @@
 package eu.pastanetwork.pastaconomy;
 
+import java.math.BigDecimal;
+
 public interface IOrderPlacer {
     public void addMarket(Market theMarket);
     public boolean hasMarket();
-    public boolean processOrder(Market.Order order, int quantity);
+    public boolean checkOrder(Market.Order theOrder, int quantity);
+    public boolean processOrder(Market.Order order, int quantity, BigDecimal price);
 }
