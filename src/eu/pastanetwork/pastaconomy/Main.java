6428d0eb0dd6152a301e43da2b10f2c3510356ae
Lucas <lucas.m.jacquet@gmail.com>
2023-01-22 00:14:37 +0100

    Modified simulation loop to start at day 1

--- src/eu/pastanetwork/pastaconomy/Main.java
+++ src/eu/pastanetwork/pastaconomy/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         City myCity = new City("Pasta-City",200000);
         System.out.println("City: " + myCity.GetCityName() + " Population: " + myCity.GetNumberPopulation() + " Compagnies: " + myCity.GetNumberCompanies());
-        for(int i=1; i < 365; i++){
+        for(int i=1; i <= 365; i++){
             System.out.println("Simulating: Day " + i);
             myCity.update();
         }
